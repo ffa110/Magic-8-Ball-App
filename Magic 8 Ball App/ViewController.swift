23212b7697aa271b4262bcdde649e8c665f38368
ffa110 <ffabunayyan@hotmail.com>
2018-10-10 20:40:26 -0400

    Initial Commit

---
+++ Magic 8 Ball App/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball App
+//
+//  Created by Fahad Abunayyan on 10/10/18.
+//  Copyright © 2018 Fahad Abunayyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var randomImageIndex : Int = 0
+    
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+
+    
+}
+
